2da90ce9c42b6def0776e31e300b460293ab38b0
hexm <627428179@qq.com>
2020-07-15 10:22:23 +0800

    进度条显示精确到小数

--- src/main/java/com/hexm/util/NumberUtil.java
+++ src/main/java/com/hexm/util/NumberUtil.java
@@ -3,6 +3,8 @@ package com.hexm.util;
 import java.math.BigDecimal;
 
 /**
+ * 数字计算
+ *
  * @author hexm
  * @date 2020/6/4
  */
@@ -10,6 +12,7 @@ public class NumberUtil {
 
     /**
      * 除
+     *
      * @param v1
      * @param v2
      * @return
@@ -18,17 +21,49 @@ public class NumberUtil {
         if (d(v1) == 0 || d(v2) == 0) {
             return 0;
         }
-        return BigDecimal.valueOf(d(v1)).divide(BigDecimal.valueOf(d(v2)), 2, BigDecimal.ROUND_DOWN).doubleValue();
+        return div(v1.doubleValue(), v2.doubleValue());
+    }
+
+    /**
+     * 除
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double div(Double v1, Double v2) {
+        if (d(v1) == 0 || d(v2) == 0) {
+            return 0;
+        }
+        return BigDecimal.valueOf(d(v1)).divide(BigDecimal.valueOf(d(v2)), 4, BigDecimal.ROUND_DOWN).doubleValue();
     }
 
     /**
      * 乘
+     *
      * @param v1
      * @param v2
      * @return
      */
     public static double mul(Double v1, Double v2) {
+        if (v1 == null || v2 == null) {
+            return 0;
+        }
         return BigDecimal.valueOf(d(v1)).multiply(BigDecimal.valueOf(d(v2))).doubleValue();
+    }
+
+    /**
+     * 乘
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double mul(Integer v1, Integer v2) {
+        if (v1 == null || v2 == null) {
+            return 0;
+        }
+        return mul(v1.doubleValue(), v2.doubleValue());
     }
 
     private static double d(Double d) {
